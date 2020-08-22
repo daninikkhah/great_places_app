e5ccee956d8f04b5855031fb51af4f37f981e34e
daninikkhah <dnikkhahbahrami@gmail.com>
2020-08-22 08:08:40 -0700

    image_input.dart -getting image from camera -height of image set to infinity

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:io';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   @override
@@ -8,6 +9,16 @@ class ImageInput extends StatefulWidget {
 
 class _ImageInputState extends State<ImageInput> {
   File _storedImage;
+
+  Future<void> _takePicture() async {
+    final _imagePicker = ImagePicker();
+    final _pickedImage =
+        await _imagePicker.getImage(source: ImageSource.camera, maxWidth: 600);
+    setState(() {
+      _storedImage = File(_pickedImage.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
@@ -15,7 +26,7 @@ class _ImageInputState extends State<ImageInput> {
       children: [
         Container(
           width: size.width / 2,
-          height: size.width / 2,
+          height: size.width / 2 * 3 / 4,
           decoration: BoxDecoration(
             border: Border.all(width: 1, color: Colors.grey),
           ),
@@ -28,11 +39,12 @@ class _ImageInputState extends State<ImageInput> {
               : Image.file(
                   _storedImage,
                   fit: BoxFit.cover,
+                  width: double.infinity,
                 ),
         ),
         Expanded(
           child: FlatButton.icon(
-            onPressed: null,
+            onPressed: _takePicture,
             icon: const Icon(Icons.camera_alt),
             label: const Text('Take Photo'),
           ),
